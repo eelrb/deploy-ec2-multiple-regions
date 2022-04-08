c1f9455096a29b87d245d34b28270b2dc06e8038
Brian Lee <35783300+eelrb@users.noreply.github.com>
2022-04-08 00:21:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 // Modules
 module "multi_region_ec2-1" {
   source  = "app.terraform.io/bleehashiorg/multi-region-ec2/aws"
-  version = "0.0.4"
+  version = "0.0.5"
 
   aws_region = var.aws_region_1
   
@@ -24,7 +24,7 @@ module "multi_region_ec2-1" {
 
 module "multi_region_ec2-2" {
   source  = "app.terraform.io/bleehashiorg/multi-region-ec2/aws"
-  version = "0.0.4"
+  version = "0.0.5"
 
   aws_region = var.aws_region_2
   
